fb1b7f4cec6a56e71c4877d8b5a00e17cb300255
PetrZavodny <petr.zavodny@chyronhego.com>
2021-11-08 07:51:28 +0100

    Initial commit fix.

---
+++ fluent_ts_base/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import {PrimaryButton} from "@fluentui/react";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <PrimaryButton>Button</PrimaryButton>
+      </header>
+    </div>
+  );
+}
+
+export default App;
